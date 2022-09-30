e9b680b1160a55f680b361963414314ec644384d
Tobias Köngeter <tobias.koengeter@bitandblack.com>
2022-09-30 10:54:25 +0200

    Updated rector

--- ecs.php
+++ ecs.php
@@ -1,22 +1,17 @@
 <?php
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters
-        ->set(Option::PATHS, [
-            __DIR__.DIRECTORY_SEPARATOR.'src',
-            __DIR__.DIRECTORY_SEPARATOR.'tests',
-        ])
-    ;
-
-    $services = $containerConfigurator->services();
-
-    $services
-        ->set(NoUnusedImportsFixer::class)
-    ;
+return static function (ECSConfig $ECSConfig): void {
+    $ECSConfig->parallel();
+    $ECSConfig->paths([
+        __DIR__,
+    ]);
+    $ECSConfig->skip([
+        __DIR__ . DIRECTORY_SEPARATOR . 'vendor',
+    ]);
+    $ECSConfig->rule(NoUnusedImportsFixer::class);
+    $ECSConfig->import(SetList::PSR_12);
 };
--- rector.php
+++ rector.php
@@ -1,22 +1,16 @@
 <?php
 
-declare(strict_types=1);
-
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    
-    $parameters
-        ->set(Option::PATHS, [
-            __DIR__.DIRECTORY_SEPARATOR.'src',
-            __DIR__.DIRECTORY_SEPARATOR.'tests',
-        ])
-        ->set(Option::AUTO_IMPORT_NAMES, true)
-        ->set(Option::IMPORT_DOC_BLOCKS, false)
-    ;
-    
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_74);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->paths([
+        __DIR__,
+    ]);
+    $rectorConfig->skip([
+        __DIR__ . DIRECTORY_SEPARATOR . 'vendor',
+    ]);
+    $rectorConfig->importNames();
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_74);
 };
